fa5c5f423242f610469c862594f0715d68eadcd9
Skyler Manzanares <skymanzanares@hotmail.com>
2016-11-20 12:48:23 -0700

    Added attribute enum, for consistency and to make it easy to change

---
+++ src/java/edu/nmt/cocoeditor/AttributeNames.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.nmt.cocoeditor;
+
+/**
+ *
+ * @author Skyler
+ */
+public enum AttributeNames {
+    SESSION_ID("sid"),
+    USER_ID("uid");
+    
+    private String key;
+    
+    private AttributeNames(String key) {
+        this.key = key;
+    }
+    
+    @Override
+    public String toString() {
+        return key;
+    }
+    
+    public String getKey() {
+        return key;
+    }
+}
